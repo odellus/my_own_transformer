299cc3c12e4c3c428846b6444c0b7b0adbf5bfef
Thomas Wood <thomas.wood@lifebio.com>
2021-07-17 03:51:59 -0700

    Added some comments.

--- model.py
+++ model.py
@@ -7,8 +7,9 @@ class SelfAttention(nn.Module):
         super().__init__()
         self.embed_dim = embed_dim
         self.attn_weights = nn.Linear(self.embed_dim, 3 * self.embed_dim, bias=False)
+
     def forward(self, input_embeds):
-        # input_embeds
+        # input_embeds is a seq_length X embed_dim matrix.
         query, key, value = self.attn_weights(input_embeds).split(self.embed_dim, dim=-1)
         w = nn.Softmax(dim=-1)(torch.matmul(query, key.transpose(-2,-1)))
         return torch.matmul(w, value)
@@ -40,14 +41,14 @@ class TransformerBlock(nn.Module):
         self.ln2 = nn.LayerNorm(embed_dim) # Do we want this or weight sharing?
 
     def forward(self, hidden_states):
-        # x is an embed_dim X attn_span matrix.
+        # hidden_states is a seq_length X embed_dim matrix.
         # This is a toy, so no batch dimension.
-        residual = hidden_states
+        residual = hidden_states # Set up skip connection.
         hidden_states = self.ln1(hidden_states)
         hidden_states = self.attn_block(hidden_states)
-        hidden_states += residual
-        residual = hidden_states
+        hidden_states += residual # Add residual
+        residual = hidden_states # Do it again.
         hidden_states = self.ln2(hidden_states)
         ff_hidden_states = self.feed_forward(hidden_states)
-        hidden_states = ff_hidden_states + residual
+        hidden_states = ff_hidden_states + residual # One more time
         return hidden_states
